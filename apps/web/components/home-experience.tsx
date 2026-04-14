35e83a62b9d45c9bd6491f477a6c0fa41add6d87
rk88g <kv300609@gmail.com>
2026-04-13 19:40:19 -0600

    Yo1.0

--- apps/web/components/home-experience.tsx
+++ apps/web/components/home-experience.tsx
@@ -569,7 +569,7 @@ export function HomeExperience({ data }: HomeExperienceProps) {
           <div className="event-gradient" />
 
           <div className="scene-copy left-copy">
-            <h1 className="scene-wordmark">LODO LAND</h1>
+            <h1 id="INICIOWEB" className="scene-wordmark"></h1>
             <h2 className="scene-title">{data.event.title}</h2>
             <p>{data.event.description}</p>
             <div className="meta-row">
